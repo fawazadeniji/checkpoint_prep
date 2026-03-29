296a36a77a7f9370f640a976cc144ed8dc032bad
Fawaz Adeniji <fawazadeniji123@gmail.com>
2026-03-29 23:52:22 +0100

    Refactor ConcatAlternate and ConcatSlice functions to remove piscine dependency and improve readability

--- 50_percent/concatalternate/main.go
+++ 50_percent/concatalternate/main.go
@@ -2,12 +2,40 @@ package main
 
 import (
 	"fmt"
-	"piscine"
 )
 
+func ConcatAlternate(slice1, slice2 []int) []int {
+	var first, second []int
+
+	// Determine which slice to start with based on length
+	if len(slice2) > len(slice1) {
+		first = slice2
+		second = slice1
+	} else {
+		first = slice1
+		second = slice2
+	}
+
+	res := make([]int, 0, len(slice1)+len(slice2))
+	
+	// Find the minimum length to know how many pairs we can make
+	minLen := len(second)
+
+	// Alternating part
+	for i := 0; i < minLen; i++ {
+		res = append(res, first[i])
+		res = append(res, second[i])
+	}
+
+	// Add the remaining elements from the longer slice
+	res = append(res, first[minLen:]...)
+
+	return res
+}
+
 func main() {
-	fmt.Println(piscine.ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(piscine.ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
-	fmt.Println(piscine.ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
-	fmt.Println(piscine.ConcatAlternate([]int{1, 2, 3}, []int{}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
 }
--- 50_percent/concatslice/main.go
+++ 50_percent/concatslice/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
-	"piscine"
 )
 
+func ConcatSlice(slice1, slice2 []int) []int {
+	// We append all elements of slice2 to slice1.
+	// The '...' suffix is essential to treat slice2 as a list of arguments.
+	return append(slice1, slice2...)
+}
+
+func ConcatSlice2(slice1, slice2 []int) []int {
+	// Create a new slice with the exact combined length
+	newSize := len(slice1) + len(slice2)
+	res := make([]int, newSize)
+
+	// Copy the first slice into the start of the result
+	copy(res, slice1)
+	
+	// Copy the second slice into the result starting after slice1
+	copy(res[len(slice1):], slice2)
+
+	return res
+}
+
 func main() {
-	fmt.Println(piscine.ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(piscine.ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
-	fmt.Println(piscine.ConcatSlice([]int{1, 2, 3}, []int{}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
 }
